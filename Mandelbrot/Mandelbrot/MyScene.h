6d14cfc88dddd806d727181901565fa08b1abbf7
PinballGraham <N/A>
2013-11-04 17:19:11 -0800

    Added Xcode project
    Created a new SpriteKit game project.

---
+++ Mandelbrot/Mandelbrot/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Mandelbrot
+//
+
+//  Copyright (c) 2013 Graham West. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Mandelbrot/Mandelbrot/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mandelbrot
+//
+//  Created by Graham West on 11/4/13.
+//  Copyright (c) 2013 Graham West. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
